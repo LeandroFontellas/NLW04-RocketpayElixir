6dd1470111f8d6e3c8904bfc4c4e05ebb25b005c
Leandro Fontellas Laurito <leandrofontellas@gmail.com>
2021-02-24 11:17:39 -0300

    aula 3 - logica inicial do deposito

---
+++ lib/rocketpay_web/controllers/accounts_controller.ex
@@ -0,0 +1,13 @@
+defmodule RocketpayWeb.AccountsController do
+  use RocketpayWeb, :controller
+
+  alias Rocketpay.Account
+
+  def deposit(connection, params) do
+    with {:ok, %Account{} = account} <- Rocketpay.deposit(params) do
+      connection
+    |> put_status(:ok)
+    |> render("update.json", account: account)
+    end
+  end
+end
