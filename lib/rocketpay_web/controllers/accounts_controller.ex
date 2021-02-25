eb28b01e6e0441e9b06ecd787b4a0e261b0f9e02
Leandro Fontellas Laurito <leandrofontellas@gmail.com>
2021-02-25 10:42:22 -0300

    aula 4 - criacao da transacao e alguns refatoramentos

--- lib/rocketpay_web/controllers/accounts_controller.ex
+++ lib/rocketpay_web/controllers/accounts_controller.ex
@@ -3,11 +3,29 @@ defmodule RocketpayWeb.AccountsController do
 
   alias Rocketpay.Account
 
+  alias Rocketpay.Accounts.Transactions.Response, as: TransactionResponse
+
+  action_fallback RocketpayWeb.FallbackController
+
   def deposit(connection, params) do
     with {:ok, %Account{} = account} <- Rocketpay.deposit(params) do
       connection
     |> put_status(:ok)
     |> render("update.json", account: account)
+    end
+  end
+  def withdraw(connection, params) do
+    with {:ok, %Account{} = account} <- Rocketpay.withdraw(params) do
+      connection
+    |> put_status(:ok)
+    |> render("update.json", account: account)
+    end
+  end
+  def transaction(connection, params) do
+    with {:ok, %TransactionResponse{} = transaction} <- Rocketpay.transaction(params) do
+      connection
+    |> put_status(:ok)
+    |> render("transaction.json", transaction: transaction)
     end
   end
 end
